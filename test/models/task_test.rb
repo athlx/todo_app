3e0b1cee3d7b4a94e23bbcf8e8b56972fbba6eba
Kode Athlx <kode@1337>
2018-10-31 16:09:55 +0400

    Validate Task description (w/ test)

--- test/models/task_test.rb
+++ test/models/task_test.rb
@@ -1,7 +1,17 @@
 require 'test_helper'
 
 class TaskTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @task = Task.new description: "Test task", 
+					 completed: true
+  end
+
+  test "should be valid" do
+    assert @task.valid?
+  end
+
+  test "description should be present" do
+	@task.description = "  "
+    assert_not @task.valid?
+  end
 end
